1e6dbc312ee09bc4e20f12bc4534bbd9131e6438
Giulio Fagioli <giulio@lotrek.it>
2017-05-31 02:14:05 +0200

    Parser, now accept parameters from url

--- core/parser.go
+++ core/parser.go
@@ -1,51 +1,47 @@
 package core
 import  "strings"
+import "strconv"
+import "fmt"
 //import "encoding/json"
 
 /*questa funzione controlla se immagine appartiene a tvl o no */
-func IsOfSite(url string) int {
+/*func IsOfSite(url string) int {
     if strings.Contains(url, "tvl.lotrek.it") {
         return 1
     } else {
         return 0
     }
-}
+}*/
 
 /* questa funzione spezzetta il parametro in url passato */
 
-func Parser (name string) ([]string) { //return integer, integer , integer, error
+func Parser (name string) (uint,uint,uint,error) { //return integer, integer , integer, error
 
-    /*dichiaro un array di ritorno con tre parametri*/
-    arr := make([]string, 3)
     /*ci separiamo la stringa passata per il carattere */
     stringSlice := strings.Split(name, "/")
     /*ora in posizoione 0 della stringslide, avremo dimensione e qualita*/
-    var dimqual = stringSlice[0]
+    var dimqual string = stringSlice[0]
     /* se togliamo dalla stringa originale con un replace il dimqual, ecco che abbiamo la urla*/
-    var url = strings.Replace(name, dimqual+"/", "", -1)
+    //var url = strings.Replace(name, dimqual+"/", "", -1)
 
-    /*prima di tutto, l'immagine appartiene a tvl ?  */
-    var is=IsOfSite(url)
-    if is != 0 {
+    dimQualityArray := strings.Split(dimqual, "_")
+    fmt.Println(dimQualityArray)
+    arrayOfInt := make([]uint, 3)
+    var err error=nil
 
-        /* se dimqual ha il parametro in più , tipo "_" si deve comportare in maniera diversa rispetto a non averlo */
-        if strings.Contains(dimqual, "_") {
-            stringSlice2 := strings.Split(dimqual, "_")
-            var  dim = stringSlice2[0]
-            var qual = stringSlice2[1]
-            arr[0] = dim
-            arr[1] = qual
-        } else {
-            arr[0] = dimqual
-            arr[1] = ""
-        }
-        arr[2] = url
-
-    } else {
-        arr[0] =""
-        arr[1] =""
-        arr[2] =""
+    var tmpr int
+    arrayOfInt[2] = 100
+    arrayOfInt[0] = 0
+    arrayOfInt[1] = 0
+    for i := 0; i <len(dimQualityArray); i++ {
+        tmpr,err=strconv.Atoi(dimQualityArray[i])
+        arrayOfInt[i]=uint(tmpr)
+        fmt.Println(arrayOfInt[i])
+        if err != nil { fmt.Println(err) }
     }
-
-    return arr
+    if err != nil {
+        fmt.Println(err)
+    }
+    return arrayOfInt[0],arrayOfInt[1],arrayOfInt[2],nil
 }
+
