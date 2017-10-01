815de94be916c58f8d48ea3c52c16ce66d481254
peterj <peterj@Peters-MBP.home>
2017-09-30 20:59:24 -0700

    Adding tests for parser.go + minor changes to the Parser method.

--- core/parser.go
+++ core/parser.go
@@ -1,33 +1,34 @@
 package core
-import  "strings"
+
+import "strings"
 import "strconv"
 
+// Parser extracts width, height and quality from the provided parameters.
+func Parser(name string) (uint, uint, uint, error) {
 
-func Parser (name string) (uint,uint,uint,error) {
+	stringSlice := strings.Split(name, "/")
 
-    stringSlice := strings.Split(name, "/")
+	var dimqual = stringSlice[0]
 
-    var dimqual string = stringSlice[0]
+	dimQualityArray := strings.Split(dimqual, "_")
+	arrayOfInt := make([]uint, 3)
 
-    dimQualityArray := strings.Split(dimqual, "_")
-    arrayOfInt := make([]uint, 3)
+	var err error
+	var tmpr int
 
-    var err error = nil
-    var tmpr int
+	arrayOfInt[0] = 0
+	arrayOfInt[1] = 0
+	arrayOfInt[2] = 100
 
-    arrayOfInt[2] = 100
-    arrayOfInt[0] = 0
-    arrayOfInt[1] = 0
-    for i := 0; i <len(dimQualityArray); i++ {
-        tmpr,err = strconv.Atoi(dimQualityArray[i])
-        arrayOfInt[i] = uint(tmpr)
-        if err != nil {
-            return 0, 0, 0, err
-        }
-    }
-    if err != nil {
-        return 0, 0, 0, err
-    }
-    return arrayOfInt[0], arrayOfInt[1], arrayOfInt[2], nil
+	for i := 0; i < len(dimQualityArray); i++ {
+		tmpr, err = strconv.Atoi(dimQualityArray[i])
+		if err != nil {
+			return 0, 0, 0, err
+		}
+		arrayOfInt[i] = uint(tmpr)
+	}
+	if err != nil {
+		return 0, 0, 0, err
+	}
+	return arrayOfInt[0], arrayOfInt[1], arrayOfInt[2], nil
 }
-
