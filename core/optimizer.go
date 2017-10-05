c0cf03d763e3988b53781607871e6fd6884c4d9a
hwchiu <hwchiu@linkernetworks.com>
2017-10-06 01:18:54 +0800

    change indent from tab to sapce again

--- core/optimizer.go
+++ core/optimizer.go
@@ -1,112 +1,112 @@
 package core
 
 import (
-	"crypto/sha1"
-	"encoding/base64"
-	"errors"
-	"fmt"
-	"github.com/nfnt/resize"
-	"image"
-	"io"
-	"net/http"
-	"os"
-	"path/filepath"
-	"sync"
+    "crypto/sha1"
+    "encoding/base64"
+    "errors"
+    "fmt"
+    "github.com/nfnt/resize"
+    "image"
+    "io"
+    "net/http"
+    "os"
+    "path/filepath"
+    "sync"
 )
 
 func Optimize(originalUrl string, imageParameters ImageParameters, pathtemp string, pathmedia string) (string, string, error) {
 
-	// Download file
-	response, err := http.Get(originalUrl)
-	if err != nil {
-		return "", "", errors.New("Error downloading file " + originalUrl)
-	}
+    // Download file
+    response, err := http.Get(originalUrl)
+    if err != nil {
+        return "", "", errors.New("Error downloading file " + originalUrl)
+    }
 
-	defer response.Body.Close()
+    defer response.Body.Close()
 
-	// Detect image type, size and last modified
-	responseType := response.Header.Get("Content-Type")
-	size := response.Header.Get("Content-Length")
-	lastModified := response.Header.Get("Last-Modified")
+    // Detect image type, size and last modified
+    responseType := response.Header.Get("Content-Type")
+    size := response.Header.Get("Content-Length")
+    lastModified := response.Header.Get("Last-Modified")
 
-	// Get Hash Name
-	hash := sha1.New()
-	hash.Write([]byte(fmt.Sprint(imageParameters.Width, imageParameters.Height, imageParameters.Quality, originalUrl, responseType, size, lastModified)))
-	newFileName := base64.URLEncoding.EncodeToString(hash.Sum(nil))
+    // Get Hash Name
+    hash := sha1.New()
+    hash.Write([]byte(fmt.Sprint(imageParameters.Width, imageParameters.Height, imageParameters.Quality, originalUrl, responseType, size, lastModified)))
+    newFileName := base64.URLEncoding.EncodeToString(hash.Sum(nil))
 
-	newImageTempPath := filepath.Join(pathtemp, newFileName)
-	newImageRealPath := filepath.Join(pathmedia, newFileName)
+    newImageTempPath := filepath.Join(pathtemp, newFileName)
+    newImageRealPath := filepath.Join(pathmedia, newFileName)
 
-	// Check if file exists
-	if _, err := os.Stat(newImageRealPath); err == nil {
-		return newImageRealPath, responseType, nil
-	}
+    // Check if file exists
+    if _, err := os.Stat(newImageRealPath); err == nil {
+        return newImageRealPath, responseType, nil
+    }
 
-	// Decode and resize
-	var reader io.Reader = response.Body
-	var newFileImg *os.File
-	var mu = &sync.Mutex{}
+    // Decode and resize
+    var reader io.Reader = response.Body
+    var newFileImg *os.File
+    var mu = &sync.Mutex{}
 
-	mu.Lock()
-	if _, err := os.Stat(newImageTempPath); err == nil {
-		return "", "", errors.New("Still elaborating")
-	}
+    mu.Lock()
+    if _, err := os.Stat(newImageTempPath); err == nil {
+        return "", "", errors.New("Still elaborating")
+    }
 
-	newFileImg, err = os.Create(newImageTempPath)
-	mu.Unlock()
+    newFileImg, err = os.Create(newImageTempPath)
+    mu.Unlock()
 
-	var img image.Image
-	var imageHandler ImageHandler
+    var img image.Image
+    var imageHandler ImageHandler
 
-	imageHandler, err = NewImageHandler(responseType)
-	if err != nil {
-		os.Remove(newImageTempPath)
-		return "", "", err
-	}
+    imageHandler, err = NewImageHandler(responseType)
+    if err != nil {
+        os.Remove(newImageTempPath)
+        return "", "", err
+    }
 
-	img, err = imageHandler.Decode(reader)
-	if err != nil {
-		os.Remove(newImageTempPath)
-		return "", "", errors.New("Error decoding response")
-	}
+    img, err = imageHandler.Decode(reader)
+    if err != nil {
+        os.Remove(newImageTempPath)
+        return "", "", errors.New("Error decoding response")
+    }
 
-	newImage := resize.Resize(imageParameters.Width, imageParameters.Height, img, resize.NearestNeighbor)
-	if err != nil {
-		os.Remove(newImageTempPath)
-		return "", "", errors.New("Error creating new image")
-	}
+    newImage := resize.Resize(imageParameters.Width, imageParameters.Height, img, resize.NearestNeighbor)
+    if err != nil {
+        os.Remove(newImageTempPath)
+        return "", "", errors.New("Error creating new image")
+    }
 
-	// Encode new image
-	err = imageHandler.Encode(newFileImg, newImage)
-	if err != nil {
-		os.Remove(newImageTempPath)
-		return "", "", errors.New("Error encoding response")
-	}
-	newFileImg.Close()
+    // Encode new image
+    err = imageHandler.Encode(newFileImg, newImage)
+    if err != nil {
+        os.Remove(newImageTempPath)
+        return "", "", errors.New("Error encoding response")
+    }
+    newFileImg.Close()
 
-	err = imageHandler.Convert(newImageTempPath, imageParameters.Quality)
-	if err != nil {
-		os.Remove(newImageTempPath)
-		return "", "", err
-	}
+    err = imageHandler.Convert(newImageTempPath, imageParameters.Quality)
+    if err != nil {
+        os.Remove(newImageTempPath)
+        return "", "", err
+    }
 
-	err = os.Rename(newImageTempPath, newImageRealPath)
-	if err != nil {
-		os.Remove(newImageTempPath)
-		return "", "", errors.New("Error moving file")
-	}
+    err = os.Rename(newImageTempPath, newImageRealPath)
+    if err != nil {
+        os.Remove(newImageTempPath)
+        return "", "", errors.New("Error moving file")
+    }
 
-	return newImageRealPath, responseType, nil
+    return newImageRealPath, responseType, nil
 
 }
 
 func BuildResponse(w http.ResponseWriter, imagePath string, contentType string) error {
-	img, err := os.Open(imagePath)
-	if err != nil {
-		return errors.New("Error reading from optimized file")
-	}
-	defer img.Close()
-	w.Header().Set("Content-Type", contentType) // <-- set the content-type header
-	io.Copy(w, img)
-	return nil
+    img, err := os.Open(imagePath)
+    if err != nil {
+        return errors.New("Error reading from optimized file")
+    }
+    defer img.Close()
+    w.Header().Set("Content-Type", contentType) // <-- set the content-type header
+    io.Copy(w, img)
+    return nil
 }
--- server.go
+++ server.go
@@ -1,75 +1,75 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"github.com/julienschmidt/httprouter"
-	"github.com/lotrekagency/piuma/core"
-	"io"
-	"log"
-	"net/http"
-	"os"
-	"os/user"
-	"path/filepath"
+    "flag"
+    "fmt"
+    "github.com/julienschmidt/httprouter"
+    "github.com/lotrekagency/piuma/core"
+    "io"
+    "log"
+    "net/http"
+    "os"
+    "os/user"
+    "path/filepath"
 )
 
 var pathtemp string
 var pathmedia string
 
 func Manager(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	var contentType string
-	var response *http.Response
+    var contentType string
+    var response *http.Response
 
-	imageParameters, err := core.Parser(ps.ByName("parameters"))
-	if err == nil {
-		img, contentType, err := core.Optimize(ps.ByName("url")[1:], imageParameters, pathtemp, pathmedia)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			err = core.BuildResponse(w, img, contentType)
-		}
-	}
+    imageParameters, err := core.Parser(ps.ByName("parameters"))
+    if err == nil {
+        img, contentType, err := core.Optimize(ps.ByName("url")[1:], imageParameters, pathtemp, pathmedia)
+        if err != nil {
+            fmt.Println(err)
+        } else {
+            err = core.BuildResponse(w, img, contentType)
+        }
+    }
 
-	if err != nil {
-		response, err = http.Get(ps.ByName("url")[1:])
-		if err != nil {
-			fmt.Println("Error downloading file " + ps.ByName("url")[1:])
-		} else {
-			var reader io.Reader = response.Body
-			contentType = response.Header.Get("Content-Type")
-			w.Header().Set("Content-Type", contentType) // <-- set the content-type header
-			io.Copy(w, reader)
-		}
-	}
+    if err != nil {
+        response, err = http.Get(ps.ByName("url")[1:])
+        if err != nil {
+            fmt.Println("Error downloading file " + ps.ByName("url")[1:])
+        } else {
+            var reader io.Reader = response.Body
+            contentType = response.Header.Get("Content-Type")
+            w.Header().Set("Content-Type", contentType) // <-- set the content-type header
+            io.Copy(w, reader)
+        }
+    }
 }
 
 func init() {
-	log.SetOutput(os.Stdout)
-	log.SetFlags(log.Lshortfile | log.Ldate | log.Ltime | log.Lmicroseconds)
+    log.SetOutput(os.Stdout)
+    log.SetFlags(log.Lshortfile | log.Ldate | log.Ltime | log.Lmicroseconds)
 }
 
 func main() {
-	usr, err := user.Current()
-	if err != nil {
-		log.Printf("[ERROR]: failed getting user [ %s ]", err)
-		os.Exit(1)
-	}
+    usr, err := user.Current()
+    if err != nil {
+        log.Printf("[ERROR]: failed getting user [ %s ]", err)
+        os.Exit(1)
+    }
 
-	// TODO: This could be passed in as an argument and/or read
-	// from an enviroment variable
-	var port = "8080"
+    // TODO: This could be passed in as an argument and/or read
+    // from an enviroment variable
+    var port = "8080"
 
-	flag.StringVar(&port, "port", port, "Port where piuma will run")
-	flag.StringVar(&pathmedia, "mediapath", filepath.Join(usr.HomeDir, ".piuma", "media"), "Media path")
+    flag.StringVar(&port, "port", port, "Port where piuma will run")
+    flag.StringVar(&pathmedia, "mediapath", filepath.Join(usr.HomeDir, ".piuma", "media"), "Media path")
 
-	flag.Parse()
+    flag.Parse()
 
-	pathtemp = filepath.Join(pathmedia, "temp")
+    pathtemp = filepath.Join(pathmedia, "temp")
 
-	os.MkdirAll(pathtemp, os.ModePerm)
-	os.MkdirAll(pathmedia, os.ModePerm)
+    os.MkdirAll(pathtemp, os.ModePerm)
+    os.MkdirAll(pathmedia, os.ModePerm)
 
-	router := httprouter.New()
-	router.GET("/:parameters/*url", Manager)
-	log.Fatal(http.ListenAndServe(":"+port, router))
+    router := httprouter.New()
+    router.GET("/:parameters/*url", Manager)
+    log.Fatal(http.ListenAndServe(":"+port, router))
 }
