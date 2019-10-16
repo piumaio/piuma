dc82776bd491a846cb0324f97f8e3943ba5f9f63
Andrea Stagi <stagi.andrea@gmail.com>
2019-10-16 15:28:56 +0200

    Fix PngQuant errors on quality

--- core/image_png.go
+++ core/image_png.go
@@ -26,15 +26,23 @@ func (p *PNGHandler) Encode(newImgFile *os.File, newImage image.Image) error {
 }
 
 func (p *PNGHandler) Convert(newImageTempPath string, quality uint) error {
-    args := []string{newImageTempPath, "-f", "--ext=.png"}
+    var err error
+    var cmd *exec.Cmd
+
+    default_args := []string{newImageTempPath, "-f", "--ext=.png", "--skip-if-larger", "--strip"}
 
     if quality != 100 {
         var qualityMin = quality - 10
         qualityParameter := fmt.Sprintf("--quality=%[1]d-%[2]d", qualityMin, quality)
-        args = append([]string{qualityParameter}, args...)
+        args := append([]string{qualityParameter}, default_args...)
+        cmd = exec.Command("pngquant", args...)
+        err = cmd.Run()
+        if err == nil {
+            return nil
+        }
     }
-    cmd := exec.Command("pngquant", args...)
-    err := cmd.Run()
+    cmd = exec.Command("pngquant", default_args...)
+    err = cmd.Run()
     if err != nil {
         return errors.New("Pngquant command not working")
     }
